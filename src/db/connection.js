7dcee3986ab9131424dd910bb07d0e6e64f931d8
KmrAnish04 <anish.1903.kmr@gmail.com>
2024-06-09 18:11:51 +0530

    Setup database and init connection

---
+++ src/db/connection.js
@@ -0,0 +1,19 @@
+import mongoose, { mongo } from "mongoose";
+import { DB_NAME } from "../constants.js";
+// import util from 'util';
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URL}/${{DB_NAME}}`);
+
+        console.log(`\nMONGODB  Connected Successfully !! \nDB HOST: ${connectionInstance.connection.host}`);
+        // console.log(util.inspect(connectionInstance, {depth: null}))
+    }
+    catch (error) {
+        console.log("MONGODB Connection Failed! \n", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
