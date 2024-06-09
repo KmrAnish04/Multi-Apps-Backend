2cc22f67dca0f37253d98929fe87c05a40d91ab5
KmrAnish04 <anish.1903.kmr@gmail.com>
2024-06-09 19:23:56 +0530

    Basic express app setup and wrote some custom error class and utils

--- src/index.js
+++ src/index.js
@@ -3,13 +3,28 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/connection.js";
+import { app } from "./app.js";
 dotenv.config({path: './env'});
 
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
 
+    // Not sure weather app has on event or not
+    // app.on("error", (error) => {
+    //     console.log("ERROR: ", error);
+    //     throw error;
+    // })
+
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("Mongo DB connection Failed !!!", error);
+})
 
 
 
